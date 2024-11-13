68fd01202c368ddf480d055ec6344f718d5e3e79
eunhwa-a11y <ttab136@gmail.com>
2024-11-13 15:38:14 +0900

    글상세페이지 수정삭제 버튼 표시

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,9 +1,16 @@
-export default function Read(props) {
-  return (
+export default async function Read(props) {
+
+  const response = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await response.json(); // json -> object
+
+   return (
     <div>
+      <h2>{topic.title}</h2>
+      {/*
       <h2>Read</h2>
       <p>parameter : {props.params.id}</p>
+      */}
+      <p>{topic.body}</p>
     </div>
-   
-  )
+  );
 }
