827e72e24f11bb46519670ce8003a72aa578f265
eunhwa-a11y <ttab136@gmail.com>
2024-11-13 10:47:48 +0900

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
+/* 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -11,6 +12,7 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+*/
 
 export const metadata = {
   title: "Create Next App",
@@ -20,8 +22,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      {/* <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+      </body> */}
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <nav>
+          <ol>
+            <li><a href="/read/1">html</a></li>
+            <li><a href="/read/2">css</a></li>
+            <li><a href="/read/3">javascript</a></li>
+          </ol>
+        </nav>
+        {children}
+        <ul>
+          <li><a href="/create">Create</a></li>
+          <li><a href="/update/1">Update</a></li>
+          <li><button>delete</button></li>
+        </ul>
       </body>
     </html>
   );
