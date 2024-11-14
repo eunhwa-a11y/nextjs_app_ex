63cdb63b078c272ad6e9645e7ff3a071bd71fe94
eunhwa-a11y <ttab136@gmail.com>
2024-11-14 09:45:25 +0900

    update 경로 업데이트

--- src/app/read/[id]/Control.js
+++ src/app/read/[id]/Control.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 import { useParams } from "next/navigation";
 
 const Control = () => {
-  const params = useParams();
+  const params = useParams(); // useParams를 통해 객체를 만듦
   const id = params.id;
   return(
   <ul>
     <li><Link href="/create">Create</Link></li>
     {id && <>
-      <li><Link href="/update/1">Update</Link></li>
+      <li><Link href={`/update/${id}`}>Update</Link></li>
       <li><button>delete</button></li>
     </>}
   </ul>
